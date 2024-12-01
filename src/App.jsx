6ceb2e707671e5b38a70c7507d7614e144763fca
tndls0720 <tndls154154@gmail.com>
2024-12-01 15:05:48 +0900

    Lazy, Suspense 사용해보기

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React, { lazy, Suspense } from 'react';
+import './App.css';
+
+const TestComponent = lazy(() => import('./TestComponent'));
+
+function App() {
+  return (
+    <div className="App">
+      <h1>
+        lazy와 Suspense
+        <br /> 사용해보기
+      </h1>
+      <Suspense fallback={<h2>로딩중...</h2>}>
+        <TestComponent />
+      </Suspense>
+    </div>
+  );
+}
+
+export default App;
